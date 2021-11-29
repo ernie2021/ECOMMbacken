d84f79e0c58a4b3297a053e1063d6f049359830e
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-29 10:36:13 -0800

    latest code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connectionn
-const sequelize = require('../ECommerceBackEnd/config/connection')
+const sequelize = require('../ECOMMbacken/config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
