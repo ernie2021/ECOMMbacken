f91ce55c5251f2634655a9a075ebd9ea7347e4fa
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-29 10:54:24 -0800

    latest code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Productss
 
   Category.findAll({
     include: Product
@@ -19,8 +16,6 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     where: {
       id: req.params.id
@@ -37,7 +32,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
   Category.create(req.body)
     .then(categoryData => {
       res.json(categoryData)
@@ -48,7 +42,6 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.update(req.body, {
     where: {
       id: req.params.id
@@ -63,7 +56,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   Category.destroy({
     where: {
       id: req.params.id
